22e759b40d7f4d5dffbc0d7c1d27b93d2e3f99cc
jefersondsgomes <jefersondsgomes@gmail.com>
2021-08-14 22:57:52 -0300

    Fixing compose build

--- app/backend/providers/sqlProvider.go
+++ app/backend/providers/sqlProvider.go
@@ -10,13 +10,13 @@ import (
 var db *gorm.DB
 
 func Connect() {
-	connection, err := gorm.Open(sqlserver.Open("server=localhost;user id=sa;password=@S0larSyst3m;port=1433;"), &gorm.Config{})
+	conn, err := gorm.Open(sqlserver.Open("server=host.docker.internal;user id=sa;password=@S0larSyst3m;port=1401"), &gorm.Config{})
 
 	if err != nil {
 		panic(fmt.Sprintf("An error occurred while trying to create database connection: %s", err.Error()))
 	}
 
-	db = connection
+	db = conn
 }
 
 func GetDB() *gorm.DB {
